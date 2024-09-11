c05ea674d4eaf84793f8f3098197965d35689d80
皮皮强 <pipiqiang@pipiqiang.cn>
2024-09-11 16:44:27 +0800

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,17 +1,16 @@
 const axios = require("axios");
-// const Host = "http://127.0.0.1:8082"
-const Host = "https://api.dydq.eu.org"
+// const Host = "http://127.0.0.1:8082/api/music"
+const Host = "https://api.dydq.eu.org/api/music"
 
 module.exports = {
     platform: "PMusic", // 插件名
-    version: "0.0.17", // 版本号
+    version: "0.0.18", // 版本号
     cacheControl: "no-store",
     srcUrl:'https://raw.kkgithub.com/ycq3/test/master/plugin.js',
     async search(query, page, type) {
         if (type === "music") {
-
             const {success, data} = (
-                await axios.get(Host + "/api/music/search", {
+                await axios.get(Host + "/search", {
                     params: {
                         query,
                         page,
@@ -29,18 +28,19 @@ module.exports = {
         }
     },
     async getLyric(musicItem) {
-        const {data} = await axios.get(Host + "/api/music/lyric", {
+        const {data} = await axios.get(Host + "/lyric", {
             params: {
                 title: musicItem.title,
                 artist: musicItem.artist,
                 album: musicItem.album,
                 id: musicItem.id,
+                source: musicItem.source,
             }
         })
 
         console.log("获取歌词",{data})
         return {
-            rawLrc: "[00:00.00] First Lyric", // 文本格式的歌词
+            rawLrc: "[00:00.00] 功能开发中", // 文本格式的歌词
         };
     },
 
@@ -48,7 +48,7 @@ module.exports = {
     async getMediaSource(musicItem, quality) {
         // 根据媒体对象获取源信息
         const fakeResult = (
-            await axios.get(Host + "/api/music/media_source", {
+            await axios.get(Host + "/media_source", {
                 params: {
                     id: musicItem.id,
                     source: musicItem.source,
@@ -67,18 +67,20 @@ module.exports = {
             url: fakeResult.data.url,
         };
     },
-    async getTopLists(){
+    async getTopLists() {
+        const {data} = (await axios.get(Host+'/top_lists')).data
+        // 获取榜单
+        console.log("获取榜单",data)
+        return data.results;
+    },
+    async importMusicSheet(url) {
+        // 导入歌单
 
-    }
+        const musicItems = (
+            await axios.get(Host+"/import_music_sheet", {
+                params: { url },
+            })
+        ).data;
+        return musicItems;
+    },
 };
-
-// module.exports.getLyric({
-//     title: '夜曲',
-//     artist: '周杰伦',
-//     album: '十一月的萧邦',
-//     id: 'https://zh.followlyrics.com/lyrics/99416/ye-qu'
-// }).then(console.log);
-
-
-// module.exports.search("周杰伦", 2, "music")
-// module.exports.search("周杰伦", 3, "music")
