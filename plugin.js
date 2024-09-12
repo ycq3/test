f84946ceaba74e7897e714fb9efe798705605856
皮皮强 <pipiqiang@pipiqiang.cn>
2024-09-12 14:28:28 +0800

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -4,9 +4,34 @@ const Host = "https://api.dydq.eu.org/api/music"
 
 module.exports = {
     platform: "PMusic", // 插件名
-    version: "0.0.18", // 版本号
+    version: "0.0.19", // 版本号
+    author: "pipiqiang",
     cacheControl: "no-store",
-    srcUrl:'https://raw.kkgithub.com/ycq3/test/master/plugin.js',
+    srcUrl: 'https://raw.kkgithub.com/ycq3/test/master/plugin.js',
+    supportedSearchType: ["music",],
+    hints: {
+        // 导入歌单浮层上的提示
+        importMusicSheet: [
+            "支持导入网易和QQ歌单",
+            "导入完成需要较长时间匹配歌曲",
+            "可以稍后到歌单页面查看结果",
+        ],
+        // 导入单曲浮层上的提示
+        importMusicItem: [
+            // "注意事项：导入单曲时需要XXX",
+            // "第二点注意事项，导入单曲时需要XXX",
+        ],
+    },
+    userVariables: [
+        {
+            key: "userId",
+            name: "用户ID",
+        },
+        {
+            key: "token",
+            name: "Token",
+        },
+    ],
     async search(query, page, type) {
         if (type === "music") {
             const {success, data} = (
@@ -38,10 +63,8 @@ module.exports = {
             }
         })
 
-        console.log("获取歌词",{data})
-        return {
-            rawLrc: "[00:00.00] 功能开发中", // 文本格式的歌词
-        };
+        console.log("获取歌词", {data})
+        return data.data
     },
 
     // 获取音源
@@ -61,24 +84,24 @@ module.exports = {
             })
         ).data;
 
-        console.log("获取音源",{fakeResult})
+        console.log("获取音源", {fakeResult})
         // 转化为插件可识别的返回值
         return {
             url: fakeResult.data.url,
         };
     },
     async getTopLists() {
-        const {data} = (await axios.get(Host+'/top_lists')).data
+        const {data} = (await axios.get(Host + '/top_lists')).data
         // 获取榜单
-        console.log("获取榜单",data)
+        console.log("获取榜单", data)
         return data.results;
     },
     async importMusicSheet(url) {
         // 导入歌单
 
         const musicItems = (
-            await axios.get(Host+"/import_music_sheet", {
-                params: { url },
+            await axios.get(Host + "/import_music_sheet", {
+                params: {url},
             })
         ).data;
         return musicItems;
